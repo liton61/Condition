7d557ba685de10f72f1750b576723df070d88091
liton61 <litonofficial61@gmail.com>
2024-04-24 13:08:05 +0600

    multi condition statement added

---
+++ multiCondition.js
@@ -0,0 +1,10 @@
+var ram = 8;
+var room = 256;
+var price = 40000;
+
+if(ram == 8 && room == 512 && price > 40000){
+    console.log("I will buy a samsung phone");
+}
+else{
+    console.log("I will buy another phone");
+}
